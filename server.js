29d0e0a1e555f4f079a591a313b89206625a050f
Zachary Gexler <zacharygexler@Zacharys-MacBook-Pro.local>
2022-09-04 15:27:45 -0400

    can't get pics to show

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const pokemon = require("./models/pokemon")
+const port = 3000;
+const methodOverride = require("method-override")
+
+app.use(methodOverride("_method"));
+
+app.use((req, res, next) => {
+    next()
+})
+
+
+app.post("/pokemon", (req, res) => {
+    console.log(req.body)
+    res.send("data received")
+})
+
+app.get("/pokemon/", function (req, res) {
+    res.render("index.ejs", {
+        allPokemon: pokemon
+    })
+});
+
+app.get("/pokemon/new", function (req, res) {
+    res.render("new.ejs")
+})
+
+app.get("/pokemon/:indexOfPokemonArray", function (req, res) {
+    res.render("show.ejs", {
+        pokemon: pokemon[req.params.indexOfPokemonArray]
+    })
+})
+
+app.delete("/pokemon:indexOfPokemonArray", (req, res) => {
+    pokemon.splice(req.params.indexOfPokemonArray) //remove the item from the array
+    res.redirect("/pokemon") //redirect back to index route
+})
+
+app.get("/pokemon/id"), function (res, req) {
+    res.render("show.ejs", {
+        pokemonId: pokemon[req.params.id]
+    }
+    )
+}
+
+
+// TELL OUR APP TO LISTEN ON PORT...
+app.listen(port, () => {
+    console.log(`listening on port `, port)
+});
---
+++ views/edit.js
@@ -0,0 +1,21 @@
+
+!<DOCTYPE html >
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>Create a new Pokemon</title>
+        </head>
+        <body>
+            <h1>New Pokemon page</h1>
+
+            <form action="/pokemon" method="POST">
+                Name: <input type="text" name="name" value="<a%pokemon.name%>" /><br />
+                ID: <input type="text" name="color" value="<a%pokemon.id%>" /><br />
+                IMG: <input type="text" name="img" value="<a%pokemon.img%>" /><br />
+                Type: <input type="text" name="type" value="<a%pokemon.type%>" /><br />
+                Attack: <input type="text" name="attack" value="<a%pokemon.attack%>" /><br />
+                Defense: <input type="text" name="defense" /><br />
+                <input type="submit" value="Create Pokemon" />
+            </form>
+        </body>
+    </html>
