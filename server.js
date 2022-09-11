b5cb38f9539fd804809127f6bd597acbc0bbe560
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-09-11 15:52:39 -0400

    back to original working with delete added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override")
 
 app.use(methodOverride("_method"));
 
+
 app.use((req, res, next) => {
     next()
 })
@@ -15,14 +16,29 @@ app.get("/pokemon/", function (req, res) {
     })
 });
 
+app.get("/pokemon/:indexOfPokemonArray", function (req, res) {
+    res.render("show.ejs", {
+        pokemon: pokemon[req.params.indexOfPokemonArray]
+    })
+})
+
+app.get("/pokemon/:new", function (req, res) {
+    res.render("new.ejs")
+  })
+  
+app.delete("/pokemon/:indexOfPokemonArray", (req, res) => {
+    pokemon.splice(req.params.indexOfPokemonArray, 1) //remove the item from the array
+    res.redirect("/pokemon") //redirect back to index route
+  })
+
 app.post("/pokemon", (req, res) => {
     console.log(req.body)
     res.send("data received")
 })
 
-app.get("/pokemon/new", function (req, res) {
-    res.render("new.ejs")
-})
+app.get("/pokemon/:new", function (req, res) {
+    res.redirect("new.ejs")
+  })
 
 app.get("/pokemon/:indexOfPokemonArray", function (req, res) {
     res.render("show.ejs", {
@@ -35,7 +51,7 @@ app.delete("/pokemon:indexOfPokemonArray", (req, res) => {
     res.redirect("/pokemon") //redirect back to index route
 })
 
-app.get("/pokemon/id"), function (res, req) {
+app.get("/pokemon/:id"), function (res, req) {
     res.render("show.ejs", {
         pokemonId: pokemon[req.params.id]
     }
@@ -47,3 +63,19 @@ app.get("/pokemon/id"), function (res, req) {
 app.listen(port, () => {
     console.log(`listening on port `, port)
 });
+
+
+// Index
+// GET /pokemon
+// Show
+// GET /pokemon/:id
+// New
+// GET /pokemon/new
+// Edit
+// GET /pokemon/:id/edit
+// Create
+// POST /pokemon
+// Update
+// PUT /pokemon/:id
+// Destroy
+// DELETE /pokemon/:id
