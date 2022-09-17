f2c017ac51859231db4f27d6267a66f76b70351f
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-09-17 15:29:00 -0400

    attempting to add CSS

--- server.js
+++ server.js
@@ -10,17 +10,22 @@ const methodOverride = require("method-override")
 //=====================
 
 const app = express();
+const port = 3000;
 
 //====================
 //MIDDLEWARE
 //====================
 
 //TELLING EXPRESS TO USE MIDDLEWARE
-  //Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
-  //The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true).
-  app.use(express.static('public'))
-  app.use(express.urlencoded({extended: false}))
-  app.use(methodOverride("_method"))
+//Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
+//The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true).
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(express.static("pokedex"));
+// using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
+// app.use("/route", express.static("foldername"));
+
 
 //====================
 //DEFINE ROUTES
@@ -40,23 +45,23 @@ app.get("/", (req, res) => {
 })
 
 //INDEX
-app.get("/pokemon", (req, res) => {
+app.get("/pokemon/", (req, res) => {
     res.render("index.ejs", {
-        pokedex: pokemon //renders index page and defines variable
+       pokedex: pokemon //renders index page and defines variable
     })
 })
 
 //NEW
 app.get("/pokemon/new", (req, res) => {
-   res.render("new.ejs", {
-    pokedex: pokemon //renders new page and defines variable
-   })
+    res.render("new.ejs", {
+        pokedex: pokemon //renders new page and defines variable
+    })
 })
 
 //DELETE
 app.delete("/pokemon/:id", (req, res) => {
-        pokemon.splice(req.params.id, 1) //splices the pokemon at indicated id from the pokemon array
-        res.redirect("/pokemon") //redirects user back to pokedex
+    pokemon.splice(req.params.id, 1) //splices the pokemon at indicated id from the pokemon array
+    res.redirect("/pokemon") //redirects user back to pokedex
 })
 
 //UPDATE
@@ -78,7 +83,8 @@ app.put("/pokemon/:id", (req, res) => {
 
     };
     pokemon[req.params.id] = newPokemon //puts pokemon at indicated index as info received from "new"
-    res.redirect("/pokemon")}) //redirects user back to pokedex
+    res.redirect("/pokemon")
+}) //redirects user back to pokedex
 
 //CREATE
 app.post("/pokemon", (req, res) => {
@@ -106,8 +112,8 @@ app.post("/pokemon", (req, res) => {
 //EDIT
 app.get("/pokemon/:id/edit", (req, res) => {
     res.render("edit.ejs", {
-    pokemon: pokemon[req.params.id],//redefines pokemon as pokemon at specific index
-    pokemonId: req.params.id//sets pokemonId equal to the exact number it is in the pokemon array
+        pokemon: pokemon[req.params.id],//redefines pokemon as pokemon at specific index
+        pokemonId: req.params.id//sets pokemonId equal to the exact number it is in the pokemon array
     })
 })
 
